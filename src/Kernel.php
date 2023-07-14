0661daa8fde41c834655303ea39ccbc8344d1055
MrAnyx <1ptitigre@gmail.com>
2023-07-14 19:13:16 +0000

    feat(core) Initial commit

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
