cc36984524514338b4abee5a880ad29956aee71b
MrAnyx <1ptitigre@gmail.com>
2023-07-23 19:18:45 +0000

    feat(coreà Added default datafixture file

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,8 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
