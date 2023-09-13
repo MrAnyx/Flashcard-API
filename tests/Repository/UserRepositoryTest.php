50fdc219d3d964608daa69cec796704d98535bcc
MrAnyx <1ptitigre@gmail.com>
2023-09-13 20:39:28 +0000

    feat(test) Added UpgradePassword test case

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -5,6 +5,16 @@ namespace App\Tests\Repository;
 use App\Model\Paginator;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class __Animal__ implements PasswordAuthenticatedUserInterface
+{
+    public function getPassword(): ?string
+    {
+        return '';
+    }
+}
 
 class UserRepositoryTest extends KernelTestCase
 {
@@ -41,5 +51,29 @@ class UserRepositoryTest extends KernelTestCase
         $resultByUsername = $userRepository->loadUserByIdentifier('Hello World!');
 
         $this->assertSame(null, $resultByUsername);
+    }
+
+    public function testUpgradePassword(): void
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = self::getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findOneBy(['username' => 'test']);
+
+        $newPassword = uniqid();
+        $userRepository->upgradePassword($user, $newPassword);
+
+        $this->assertSame($newPassword, $user->getPassword());
+    }
+
+    public function testUpgradePasswordUnsupportedUser(): void
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = self::getContainer()->get(UserRepository::class);
+
+        $entity = new __Animal__();
+
+        $this->expectException(UnsupportedUserException::class);
+        $userRepository->upgradePassword($entity, 'new password');
     }
 }
