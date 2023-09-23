7d2414268759b73fe7b08ca9624a5a8b2ccb64a5
MrAnyx <1ptitigre@gmail.com>
2023-09-23 22:08:47 +0000

    feat(security) Added logout route to invalidate refresh token

--- src/Controller/User/SecurityController.php
+++ src/Controller/User/SecurityController.php
@@ -2,10 +2,23 @@
 
 namespace App\Controller\User;
 
+use Exception;
+use App\Entity\User;
+use App\Exception\ApiException;
+use App\Service\RequestPayloadService;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\AbstractRestController;
+use App\OptionsResolver\UserOptionsResolver;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
 
-#[Route('/api/auth', 'api_', format: 'json')]
+#[Route('/api/auth', 'api_auth_', format: 'json')]
 class SecurityController extends AbstractRestController
 {
     #[Route('/login', name: 'login', methods: ['POST'])]
@@ -14,9 +27,67 @@ class SecurityController extends AbstractRestController
         // This method is not executed
     }
 
+    #[Route('/logout', name: 'logout', methods: ['POST'])]
+    public function logout(): void
+    {
+        // This method is not executed
+    }
+
     #[Route('/token/refresh', name: 'token_refresh', methods: ['POST'])]
     public function tokenRefresh(): void
     {
         // This method is not executed
+    }
+
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher,
+        ValidatorInterface $validator,
+        UserOptionsResolver $userOptionsResolver,
+        RequestPayloadService $requestPayloadService,
+        JWTTokenManagerInterface $jwtManager,
+        RefreshTokenGeneratorInterface $refreshTokenGenerator
+    ): JsonResponse {
+
+        try {
+            // Retrieve the request body
+            $body = $requestPayloadService->getRequestPayload($request);
+
+            // Validate the content of the request body
+            $data = $userOptionsResolver
+                ->configureUsername(true)
+                ->configureEmail(true)
+                ->configurePassword(true)
+                ->resolve($body);
+        } catch (Exception $e) {
+            throw new ApiException($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        // Temporarly create the element
+        $user = new User();
+        $user
+            ->setEmail($data['email'])
+            ->setUsername($data['username'])
+            ->setRawPassword($data['password']);
+
+        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+
+        // Second validation using the validation constraints
+        $this->validateEntity($user, ['Default', 'edit:user:password']);
+
+        // Save the new element
+        $em->persist($user);
+        $em->flush();
+
+        // Return the element with the the status 201 (Created)
+        return $this->json(
+            [
+                'token' => $jwtManager->create($user),
+                'refresh_token' => $refreshTokenGenerator->createForUserWithTtl($user, $this->getParameter('gesdinet_jwt_refresh_token.ttl'))->getRefreshToken(),
+            ],
+            Response::HTTP_CREATED,
+        );
     }
 }
