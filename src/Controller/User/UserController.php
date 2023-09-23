38dcf04514630f9480e5cc44b3326d7fb31d6bcd
MrAnyx <1ptitigre@gmail.com>
2023-09-23 22:09:04 +0000

    fix(user) Removed not null condition

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -3,9 +3,7 @@
 namespace App\Controller\User;
 
 use App\Service\EntityChecker;
-use App\Exception\ApiException;
 use App\Controller\AbstractRestController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api', 'api_', format: 'json')]
@@ -15,10 +13,6 @@ class UserController extends AbstractRestController
     public function getMe(EntityChecker $entityChecker)
     {
         $user = $this->getUser();
-
-        if ($user === null) {
-            throw new ApiException('You must be logged in before using this route', Response::HTTP_UNAUTHORIZED);
-        }
 
         return $this->json($user, context: ['groups' => ['read:user:user']]);
     }
