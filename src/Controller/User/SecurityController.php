c0215ed70849a16ab0ae2523d376445bb94cb6c5
MrAnyx <1ptitigre@gmail.com>
2023-09-24 21:32:08 +0000

    feat(auth) Added registration route

--- src/Controller/User/SecurityController.php
+++ src/Controller/User/SecurityController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller\User;
 
+use Exception;
 use App\Entity\User;
 use App\Exception\ApiException;
+use App\Service\TokenGenerator;
+use App\Service\RequestPayloadService;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\AbstractRestController;
+use App\OptionsResolver\UserOptionsResolver;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/api/auth', 'api_auth_', format: 'json')]
 class SecurityController extends AbstractRestController
@@ -25,5 +33,55 @@ class SecurityController extends AbstractRestController
         return $this->json([
             'token' => $user->getToken(),
         ]);
+    }
+
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher,
+        ValidatorInterface $validator,
+        UserOptionsResolver $userOptionsResolver,
+        RequestPayloadService $requestPayloadService,
+        TokenGenerator $tokenGenerator
+    ): JsonResponse {
+        try {
+            // Retrieve the request body
+            $body = $requestPayloadService->getRequestPayload($request);
+
+            // Validate the content of the request body
+            $data = $userOptionsResolver
+                ->configureUsername(true)
+                ->configureEmail(true)
+                ->configurePassword(true)
+                ->resolve($body);
+        } catch (Exception $e) {
+            throw new ApiException(Response::HTTP_BAD_REQUEST, $e->getMessage());
+        }
+
+        // Temporarly create the element
+        $user = new User();
+        $user
+            ->setEmail($data['email'])
+            ->setUsername($data['username'])
+            ->setToken($tokenGenerator->generateToken())
+            ->setRawPassword($data['password']);
+
+        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+
+        // Second validation using the validation constraints
+        $this->validateEntity($user, ['Default', 'edit:user:password']);
+
+        // Save the new element
+        $em->persist($user);
+        $em->flush();
+
+        // Return the element with the the status 201 (Created)
+        return $this->json(
+            [
+                'token' => $user->getToken(),
+            ],
+            Response::HTTP_CREATED,
+        );
     }
 }
