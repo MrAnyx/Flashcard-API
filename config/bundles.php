d70c723f7c62588d1bb96ba9855a7b5f7a6f739e
MrAnyx <1ptitigre@gmail.com>
2023-09-24 16:54:42 +0000

    fix(auth) Removed jwt token authentication

--- config/bundles.php
+++ config/bundles.php
@@ -8,8 +8,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Controller/User/SecurityController.php
+++ src/Controller/User/SecurityController.php
@@ -2,21 +2,8 @@
 
 namespace App\Controller\User;
 
-use Exception;
-use App\Entity\User;
-use App\Exception\ApiException;
-use App\Service\RequestPayloadService;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\AbstractRestController;
-use App\OptionsResolver\UserOptionsResolver;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
-use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
 
 #[Route('/api/auth', 'api_auth_', format: 'json')]
 class SecurityController extends AbstractRestController
@@ -31,68 +18,5 @@ class SecurityController extends AbstractRestController
     public function logout(): void
     {
         // This method is not executed
-    }
-
-    #[Route('/token/refresh', name: 'token_refresh', methods: ['POST'])]
-    public function tokenRefresh(): void
-    {
-        // This method is not executed
-    }
-
-    #[Route('/register', name: 'register', methods: ['POST'])]
-    public function register(
-        Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordHasher,
-        ValidatorInterface $validator,
-        UserOptionsResolver $userOptionsResolver,
-        RequestPayloadService $requestPayloadService,
-        JWTTokenManagerInterface $jwtManager,
-        RefreshTokenGeneratorInterface $refreshTokenGenerator
-    ): JsonResponse {
-
-        try {
-            // Retrieve the request body
-            $body = $requestPayloadService->getRequestPayload($request);
-
-            // Validate the content of the request body
-            $data = $userOptionsResolver
-                ->configureUsername(true)
-                ->configureEmail(true)
-                ->configurePassword(true)
-                ->resolve($body);
-        } catch (Exception $e) {
-            throw new ApiException(Response::HTTP_BAD_REQUEST, $e->getMessage());
-        }
-
-        // Temporarly create the element
-        $user = new User();
-        $user
-            ->setEmail($data['email'])
-            ->setUsername($data['username'])
-            ->setRawPassword($data['password']);
-
-        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
-
-        // Second validation using the validation constraints
-        $this->validateEntity($user, ['Default', 'edit:user:password']);
-
-        // Save the new element
-        $em->persist($user);
-
-        $refreshToken = $refreshTokenGenerator->createForUserWithTtl($user, $this->getParameter('gesdinet_jwt_refresh_token.ttl'));
-        $em->persist($refreshToken);
-
-        $em->flush();
-
-        // Return the element with the the status 201 (Created)
-        return $this->json(
-            [
-                'token' => $jwtManager->create($user),
-                'refresh_token' => $refreshToken->getRefreshToken(),
-                'refresh_token_expiration' => $refreshToken->getValid()->getTimestamp(),
-            ],
-            Response::HTTP_CREATED,
-        );
     }
 }
