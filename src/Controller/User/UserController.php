43ec31e0076b1ea10d2c3f99d5b4d1308227192a
MrAnyx <1ptitigre@gmail.com>
2023-10-10 20:49:01 +0000

    feat(user) Added user deletion route

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\User;
 
+use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\AbstractRestController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api', 'api_', format: 'json')]
@@ -14,5 +16,17 @@ class UserController extends AbstractRestController
         $user = $this->getUser();
 
         return $this->json($user, context: ['groups' => ['read:user:user']]);
+    }
+
+    #[Route('/users/me', name: 'delete_me', methods: ['DELETE'])]
+    public function deleteMe(EntityManagerInterface $em)
+    {
+        $user = $this->getUser();
+
+        // All flashcards, units, topics and reviews are deleted using the on cascade trigger
+        $em->remove($user);
+        $em->flush();
+
+        return $this->json(null, status: Response::HTTP_NO_CONTENT);
     }
 }
