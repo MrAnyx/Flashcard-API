94f55acc382d73b584e51d355658ff428e7836c1
MrAnyx <1ptitigre@gmail.com>
2024-01-19 22:28:58 +0000

    fix(cs) Updated phpdoc on UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -14,8 +14,6 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 /**
  * @extends ServiceEntityRepository<User>
  *
- * @implements PasswordUpgraderInterface<User>
- *
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[] findAll()
