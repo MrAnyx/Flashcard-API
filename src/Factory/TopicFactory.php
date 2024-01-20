de17c2f0e935f4cfd0bfe4c82695867a8abde8db
MrAnyx <1ptitigre@gmail.com>
2024-01-20 21:42:43 +0000

    doc(factory) removed useless phpdoc

--- src/Factory/TopicFactory.php
+++ src/Factory/TopicFactory.php
@@ -47,8 +47,6 @@ final class TopicFactory extends ModelFactory
 {
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
      */
     public function __construct()
     {
@@ -57,8 +55,6 @@ final class TopicFactory extends ModelFactory
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
      */
     protected function getDefaults(): array
     {
