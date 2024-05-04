62f60fefeb8e30b080fbceadb70222b0e65fcf61
MrAnyx <1ptitigre@gmail.com>
2024-05-04 19:00:46 +0000

    refacto(events) Updated listener to subscriber

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -2,27 +2,34 @@
 
 declare(strict_types=1);
 
-namespace App\EventListener;
+namespace App\EventSubscriber;
 
 use App\Enum\JsonStandardStatus;
 use App\Model\ErrorStandard;
 use App\Model\JsonStandard;
-use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-#[AsEventListener]
-class ExceptionListener
+class ExceptionSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         public NormalizerInterface $normalizer
     ) {
     }
 
-    public function __invoke(ExceptionEvent $event): void
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
         $statusCode = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
@@ -42,5 +49,6 @@ class ExceptionListener
             $statusCode);
 
         $event->setResponse($response);
+        $event->stopPropagation();
     }
 }
