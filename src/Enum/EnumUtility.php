12bd20291d717bf8553a54bcf4fc9870c8d25514
MrAnyx <1ptitigre@gmail.com>
2024-07-30 15:27:41 +0000

    feat(enum) Add EnumUtility to provide additional features on enums

---
+++ src/Enum/EnumUtility.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+trait EnumUtility
+{
+    public static function names(): array
+    {
+        return array_column(self::cases(), 'name');
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function array(): array
+    {
+        return array_combine(self::names(), self::values());
+    }
+
+    public static function arrayCases(): array
+    {
+        return array_combine(self::names(), self::cases());
+    }
+}
