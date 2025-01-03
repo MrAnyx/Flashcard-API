36445a6fe603092d831b42ae31f85f00ad16b70f
MrAnyx <1ptitigre@gmail.com>
2025-01-03 00:30:43 +0000

    fix: Update entrypoint script to use user mode for systemd daemon reload

--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -6,7 +6,7 @@ echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 # Enable and start the worker
-systemctl daemon-reload
+systemctl --user daemon-reload
 systemctl --user enable messenger-worker@1
 systemctl --user start messenger-worker@1
 
