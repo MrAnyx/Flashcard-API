5f977c806cd9ca96b31efad29777c65b2154cadc
MrAnyx <gentle.job6107@fastmail.com>
2025-01-03 23:17:34 +0000

    fix: Remove sourcing of environment variables from entrypoint script and update supervisord configuration to use EnvironmentFile

--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Source environment variables
-echo "Sourcing environment variables..."
-. /etc/environment
-
 # Run database migrations
 echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
